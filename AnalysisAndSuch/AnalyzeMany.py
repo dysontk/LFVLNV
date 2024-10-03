d99c697cf0f321882da63539ff3f212e695dac1c
dysontk <dkennedy@umass.edu>
2024-10-03 10:21:14 -0600

    Made analyze many

---
+++ AnalysisAndSuch/AnalyzeMany.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def run_command(command):
+    try:
+        output = subprocess.check_output(command, shell=True, encoding='utf8', stderr=subprocess.STDOUT)
+        print(f"Output of command '{command}' is",  f'{output}', sep='\n')
+    except subprocess.CalledProcessError:
+        # print("Error. Generation probably failed")
+        output = str(f"ERROR: Something when wrong when running {command}")
+    return output
+
+
+if __name__=='__main__':
+
+    list_of_data_types = ['LNVF']
+    AnalyzeCommand = './JetFake/main '
+
+    list_of_files = []
+
+    for dtyp in list_of_data_types:
+        theseFiles = run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{dtyp}/Events/*/*delphes_events.root')
+        list_of_files.append(theseFiles.replace('\n', ' '))
+        print(theseFiles.replace('\n', ' '))
+
+
+    for file in list_of_files:
+        AnalyzeCommand += file
+
+    print(AnalyzeCommand)
