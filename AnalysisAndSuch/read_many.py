4359a8393f587bb8418f097632906fe32c14579d
dysontk <dkennedy@umass.edu>
2024-10-07 12:24:33 -0600

    troubleshooting read_many

--- AnalysisAndSuch/read_many.py
+++ AnalysisAndSuch/read_many.py
@@ -27,10 +27,10 @@ if __name__ == '__main__':
     to_print = 'Number of Events Generated:'
     for typ in eventTypes:
         to_print += '\n' + typ + ': '
-        files = GMOU.run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{self.event}/Events/*/*delphes_events.root').split('\n')
+        files = run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{self.event}/Events/*/*delphes_events.root').split('\n')
         eventCounts.append(0)
         for ThisFile in files:
-            eventCounts[-1]+= GMOU.find_num_gend(ThisFile)
+            eventCounts[-1]+= find_num_gend(ThisFile)
         to_print += eventCounts[eventTypes.index(typ)]
     
     print(to_print)
