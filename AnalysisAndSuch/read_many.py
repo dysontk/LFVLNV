1058c56ab22bd29cbba5076b0b26f9e3b4f0b86b
dysontk <dkennedy@umass.edu>
2024-10-07 12:26:05 -0600

    troubleshooting read_many

--- AnalysisAndSuch/read_many.py
+++ AnalysisAndSuch/read_many.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     to_print = 'Number of Events Generated:'
     for typ in eventTypes:
         to_print += '\n' + typ + ': '
-        files = run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{self.event}/Events/*/*delphes_events.root').split('\n')
+        files = run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{typ}/Events/*/*delphes_events.root').split('\n')
         eventCounts.append(0)
         for ThisFile in files:
             eventCounts[-1]+= find_num_gend(ThisFile)
