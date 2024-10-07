d0629765539695de86e1061aaeebbd40249dd854
dysontk <dkennedy@umass.edu>
2024-10-07 12:23:54 -0600

    troubleshooting read_many

--- AnalysisAndSuch/read_many.py
+++ AnalysisAndSuch/read_many.py
@@ -1,6 +1,21 @@
 import sys, os
-sys.path.append(os.path.abspath('/home/dysontravis/Research/LFVLNV/GenerationFiles/'))
-import GenManyOnUnity.py as GMOU
+import subprocess
+# sys.path.append(os.path.abspath('/home/dysontravis/Research/LFVLNV/GenerationFiles/'))
+# import GenManyOnUnity as GMOU
+
+def run_command(command):
+    try:
+        output = subprocess.check_output(command, shell=True, encoding='utf8', stderr=subprocess.STDOUT)
+        print(f"Output of command '{command}' is",  f'{output}', sep='\n')
+    except subprocess.CalledProcessError:
+        print("Error. Generation probably failed")
+        output = str(f"Something when wrong when running {command}")
+    return output
+
+def find_num_gend(fi):
+        output = run_command(f"./read_root_file {fi}")
+        m = re.search(r'\d+$', output)
+        return int(m.group()) if m else 0
 
 # def find_
 
