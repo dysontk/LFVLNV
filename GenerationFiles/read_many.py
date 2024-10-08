b618fcfe758312bed68e93ca07ecb44cf9d8d5ad
dysontk <dkennedy@umass.edu>
2024-10-08 13:59:49 -0600

    changin read many to be faster

--- GenerationFiles/read_many.py
+++ GenerationFiles/read_many.py
@@ -53,5 +53,5 @@ if __name__ == '__main__':
 
     eventTypes = ['ttbar', 'W3j', 'WZ2j', 'ZZ2j']
     
-    print(most_recent_run_num('ttbar'))
+    print(type(most_recent_run_num('ttbar')))
     # countEvents(eventTypes)
