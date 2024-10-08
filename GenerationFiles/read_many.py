d745f69fb71485c8699b5b4398ad3c286d8f0bd9
dysontk <dkennedy@umass.edu>
2024-10-08 14:33:43 -0600

    changin read many to be faster

--- GenerationFiles/read_many.py
+++ GenerationFiles/read_many.py
@@ -25,33 +25,82 @@ def find_number_in_string(strin):
 def most_recent_run_num(eventType):
     runs = GMOU.run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{eventType}/Events/').split('\n')[:-1]
     # print(len(runs))
-    print(runs)
-    return find_number_in_string(runs[-1])
+    # print(truns)
+    return find_number_in_string(runs[-1])[0]
     # runs = runs[:len(runs)/2]
     # print("hi")
     
 
-def countEvents(eventTypes, outfile):
+def countEvents(eventTypes, OF):
     # eventTypes = ['LNVF', 'ttbar', 'W3j', 'WZ2j', 'ZZ2j']
     eventCounts = []
 
+    # to_write =
     to_print = 'Number of Events Generated:'
     for typ in eventTypes:
+        if typ == '':
+            continue
+        # to_write = 
         to_print += '\n' + typ + ': '
         files = GMOU.run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{typ}/Events/*/*delphes_events.root').split('\n')
         eventCounts.append(0)
         for ThisFile in files:
             eventCounts[-1]+= GMOU.find_num_gend(ThisFile)
-        to_print += str(eventCounts[eventTypes.index(typ)])
+        NEVENTS = eventCounts[eventTypes.index(typ)]
+        to_print += str(NEVENTS)
+        # to_write += 
+        OF.write(typ + ',' + most_recent_run_num(typ) + ',' + str(NEVENTS) + '\n')
+
     
     print(to_print)
     return eventCounts
 
+def read_num_events(InFil):
+    lines = infile.readlines()
+    for l in range(len(lines)):
+        lines[l] = lines[l].split()
+    print(lines)
+    return lines
+
+
 if __name__ == '__main__':
 
     fullRecheck = False
-
     eventTypes = ['ttbar', 'W3j', 'WZ2j', 'ZZ2j']
+    fullCheckTypes = []
+    need_to_full_check = []
+    for i in range(len(eventTypes)):
+        need_to_full_check.append(False)
+    infile = open('event_counts.txt', 'r')
+
+    file_info = read_num_events(infile)
+
+    for typ in eventTypes:
+        for TYP in file_info:
+            if TYP[0]!=typ:
+                continue
+            else:
+                curr_run_max = most_recent_run_num(typ)
+                if curr_run_max != TYP[1]:
+                    need_to_full_check[eventTypes.index(typ)] = True
+                    print(f"Records show {TYP[1]} runs", f"There are {curr_run_max}")
+                else:
+                    print(f'No need to full check {typ}')
     
-    print(type(most_recent_run_num('ttbar')))
-    # countEvents(eventTypes)
+    for i in range(len(need_to_full_check)):
+        if need_to_full_check[i]:
+            eventTypes[i] = ''
+        else:
+            continue
+    
+    outfile = open('event_counts.txt', 'w')    
+    print(eventTypes)
+
+    countEvents(eventTypes, outfile)
+
+    for i in range(len(need_to_full_check)):
+        if not need_to_full_check[i]:
+            to_write = ''
+            for j in range(3):
+                to_write += str(file_info[i][j]) + ','
+            outfile.write(to_write+'\n')
