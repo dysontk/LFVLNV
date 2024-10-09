59b024f88c0d5880c3d22201bf0e01471735bd0d
dysontk <dkennedy@umass.edu>
2024-10-09 12:11:18 -0600

    changin read many to be faster

--- GenerationFiles/read_many.py
+++ GenerationFiles/read_many.py
@@ -114,15 +114,15 @@ if __name__ == '__main__':
     It then writes that information to the outfile (OF) like so:
     <type>,<highest run number>,<total n events>\n
     '''
-    temp = []
-    for i in range(len(need_to_full_check)):
-        print(int(need_to_full_check[i,1]))
-        # print(need_to_full_check[])
-        if int(need_to_full_check[i,1]):
-            temp.append(need_to_full_check[i,0])
+    # temp = []
+    # for i in range(len(need_to_full_check)):
+    #     print(int(need_to_full_check[i,1]))
+    #     # print(need_to_full_check[])
+    #     if int(need_to_full_check[i,1]):
+    #         temp.append(need_to_full_check[i,0])
                
-    print(temp)
-    print([need_to_full_check[i,0] if bool(need_to_full_check[i,1]) else '' for i in range(len(need_to_full_check))])
+    # print(temp)
+    print([need_to_full_check[i,0] if int(need_to_full_check[i,1]) else '' for i in range(len(need_to_full_check))])
     # countEvents([need_to_full_check[i,0] if need_to_full_check[i,1] else continue for i in range(len(need_to_full_check))], outfile) 
 
     '''
