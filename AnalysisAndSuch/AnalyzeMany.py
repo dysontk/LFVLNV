311c8eec49febbf32082623d2db8ece083dd2bf9
dysontk <dkennedy@umass.edu>
2024-10-14 16:07:24 -0600

    Making analyze many

--- AnalysisAndSuch/AnalyzeMany.py
+++ AnalysisAndSuch/AnalyzeMany.py
@@ -1,4 +1,5 @@
 import subprocess
+import numpy as np
 
 def run_command(command):
     try:
@@ -9,22 +10,58 @@ def run_command(command):
         output = str(f"ERROR: Something when wrong when running {command}")
     return output
 
+def find_files(typ):
+    return run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{dtyp}/Events/*/*delphes_events.root').replace('\n', ' ')
+
+def single_analysis(typ):
+    from_analysis = run_command(f'/home/dkennedy_umass_edu/LNV/MG5_aMC_v3_5_4/MyFiles/LFVLNV/AnalysisAndSuch/JetFake/main {typ} '+ find_files(typ))
+
+    theNumbersProcessing = re.search(r'Cut\n+\d+\n+\d+\n+\d+\n+\d+\n+\d+\n').group()
+    return [int(number) for number in theNumbersProcessing]
+
+
+def multi_analysis(typList):
+
+    cutNumbers = {}
+    for e_typ in typList:
+        cutNumbers.update(e{_typ:single_analysis(e_typ)})
+    
+    return cutNumbers
+
+
+def get_significance(cutNum)
+    
+    crossX = {'LNVF':0.0001279,
+              'WZ2j':0.09764,
+              'ZZ2j':0.005395,
+              'W3j':409.9,
+              'ttbar':22.74} #pb
+    intd_lumin = 0.139 #pb^-1
+    
+    BorS = {}
+    
+    for typ in cutNum:
+        BorS.update({typ:np.array[ev_count/cutNum[typ][0]*crossX[typ]*intd_lumin for ev_count in cutNum[typ]]})
+    
+    B = np.zeros(len(BorS['LNVF']))
+    # S = 0
+    for typ in BorS:
+        if typ == 'LNVF':
+            # S += BorS[typ]
+            continue
+        else:
+            B += BorS[typ]
+    return cutNum['LNVF']/(cutNum['LNVF']+B)**(1/2)
+
+
+def main():
+    list_of_data_types = ['LNVF', 'WZ2j']
+    
+    nEventsByCuts = multi_analysis(list_of_data_types)
+
+    sig_arr = get_significance(nEventsByCuts)
+
+    print(sig_arr)
 
 if __name__=='__main__':
-
-    list_of_data_types = ['LNVF', 'WZ2j']
-    Analyze = './JetFake/main '
-
-    list_of_files = []
-
-    for dtyp in list_of_data_types:
-        theseFiles = run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{dtyp}/Events/*/*delphes_events.root')
-        list_of_files.append(theseFiles.replace('\n', ' '))
-        # print(theseFiles.replace('\n', ' '))
-
-
-    for file in list_of_files:
-        Analyze += file
-
-    print('Running: ', Analyze, sep='\n')
-    run_command(Analyze)
+    main()
