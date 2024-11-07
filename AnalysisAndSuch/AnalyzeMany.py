d70206ca5c0a8467888a543b3bc09e557b1189a6
dysontk <dkennedy@umass.edu>
2024-11-07 12:14:52 -0700

    Analyze many making the stacks

--- AnalysisAndSuch/AnalyzeMany.py
+++ AnalysisAndSuch/AnalyzeMany.py
@@ -19,7 +19,7 @@ def find_files(typ):
     return run_command(f'ls /work/pi_mjrm_umass_edu/LNV_collider/Generated/{typ}/Events/*/*delphes_events.root', True).replace('\n', ' ')
 
 def single_analysis(typ):
-    from_analysis = run_command(f'/home/dkennedy_umass_edu/LNV/MyFiles/LFVLNV/AnalysisAndSuch/JetFake/main {typ} '+ find_files(typ), True)
+    from_analysis = run_command(f'/home/dkennedy_umass_edu/LNV/MyFiles/LFVLNV/AnalysisAndSuch/JetFake/main '+ find_files(typ), True)
     print("FROM ANALYSIS")
     print(from_analysis)
     theNumbersProcessing = re.search(r'Cut\n+\d+\n+\d+\n+\d+\n+\d+\n+\d+\n', from_analysis).group().split('\n')[1:-1]
