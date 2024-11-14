605313bde6bb7d9cb61e535af791ea5166593307
dysontk <dkennedy@umass.edu>
2024-11-14 10:53:28 -0700

    getting JF/main to work

--- AnalysisAndSuch/JetFake/main.cpp
+++ AnalysisAndSuch/JetFake/main.cpp
@@ -751,7 +751,7 @@ int main(int argc, const char * argv[])
         c1->SaveAs(FullPathM2jW);
         cout << "boutta save root file" <<endl;
         cout << FullPathM2jW_root << endl;
-        c1->SaveAs(FullPathM2jW_root)
+        c1  ->SaveAs(FullPathM2jW_root);
         // new TFile("test/plots/Mass_2jW.root", "RECREATE");
         MW2j->WriteObject();
 
