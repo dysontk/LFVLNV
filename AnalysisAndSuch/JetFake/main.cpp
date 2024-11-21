6329fc5b7fdaab981bf4127911f325fa07388231
dysontk <dkennedy@umass.edu>
2024-11-21 11:56:35 -0700

    histo editing attempts

--- AnalysisAndSuch/JetFake/main.cpp
+++ AnalysisAndSuch/JetFake/main.cpp
@@ -755,7 +755,7 @@ int main(int argc, const char * argv[])
         // cout << FullPathM2jW_root << endl;
         // c1->SaveAs(FullPathM2jW_root);
         new TFile("test/plots/Mass_2jW.root", "RECREATE");
-        MW2j->Write();
+        MW2j->WriteObject();
 
         cout << "Done Wj" << endl;
 
