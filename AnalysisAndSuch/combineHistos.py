cc4373b31038530686764c0accce09f5711a97c7
dysontk <dkennedy@umass.edu>
2024-11-22 12:29:54 -0700

    using for loop combin

--- AnalysisAndSuch/combineHistos.py
+++ AnalysisAndSuch/combineHistos.py
@@ -8,6 +8,7 @@ def combineHistos(start_dir=init_dir):
 
     Files = []
     for typ in eventTypes:
+        print(type(typ))
         Files.append(ROOT.TFile.Open(start_dir+typ+'/plots/Mass_2jW.root'))
     
     LNVHisto = Files[0].Get('Inv_Mass_2Jets_close_to_W')
