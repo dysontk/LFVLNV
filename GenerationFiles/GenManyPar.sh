6388f1ebc1d3124c7fe7baadc87af72952efb6e5
dysontk <dkennedy@umass.edu>
2024-11-22 09:29:27 -0700

    cleaning up

--- GenerationFiles/GenManyPar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -c 4  # Number of Cores per Task
-#SBATCH --mem=8192  # Requested Memory
-#SBATCH -p gpu  # Partition
-#SBATCH -G 1  # Number of GPUs
-#SBATCH -t 01:00:00  # Job time limit
-#SBATCH -o slurm-%j.out  # %j = job ID
-
-python3 GenManyOnUnityParallel.py
