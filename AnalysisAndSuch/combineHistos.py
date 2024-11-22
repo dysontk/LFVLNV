4c4a1963300241f46d1fd143dc647661fe6bc9c3
dysontk <dkennedy@umass.edu>
2024-11-22 12:09:27 -0700

    using for loop combin

--- AnalysisAndSuch/combineHistos.py
+++ AnalysisAndSuch/combineHistos.py
@@ -3,19 +3,19 @@ import ROOT
 init_dir = '/work/pi_mjrm_umass_edu/LNV_collider/AnalysisOutput/'
 def combineHistos(eTypes, start_dir=init_dir):
 
-    histos = []
+    histos = {}
     for typ in eTypes:
         
         File = ROOT.TFile.Open(start_dir+'LNVF/plots/Mass_2jW.root')
     # File2 = ROOT.TFile.Open(start_dir+'ZZ2j/plots/Mass_2jW.root')
 
-        histos.append(File.Get('Inv_Mass_2Jets_close_to_W'))
+        histos.update({typ:File.Get('Inv_Mass_2Jets_close_to_W')})
 
     # histo2 = File2.Get('Inv_Mass_2Jets_close_to_W')
 
     Stack = ROOT.THStack('WjPair', 'Inv_Mass_2Jets_close_to_W')
     for histo in histos:
-        Stack.Add(histo)
+        Stack.Add(histos[histo])
     # Stack.Add(histo2)
 
     can = ROOT.TCanvas('Canvas')
