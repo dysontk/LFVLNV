08106ed1ba5c5ac761c73bd9ba94c153e5b5929a
dysontk <dkennedy@umass.edu>
2024-12-09 14:14:08 -0700

    Genmany time limit 30 hr

--- GenerationFiles/GenMany.sh
+++ GenerationFiles/GenMany.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=8192  # Requested Memory
 #SBATCH -p gpu  # Partition
 #SBATCH -G 1  # Number of GPUs
-#SBATCH -t 20:00:00  # Job time limit
+#SBATCH -t 30:00:00  # Job time limit
 #SBATCH -o slurm-%j.out  # %j = job ID
 
 python3 -u /home/dkennedy_umass_edu/LNV/MyFiles/LFVLNV/GenerationFiles/GenManyOnUnity.py
