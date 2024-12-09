a6fc61af51b5a7a3b636a2f3feabda84eaa78653
dysontk <dkennedy@umass.edu>
2024-12-09 14:11:47 -0700

    Changed run files to 60k each. removed troubleshoooting lines from GENMAN

--- GenerationFiles/GenManyOnUnity.py
+++ GenerationFiles/GenManyOnUnity.py
@@ -208,9 +208,9 @@ if __name__ == '__main__':
     print(event_count_dict, type(event_count_dict), sep='\n')
     # runs2Basked = np.array([1 if not i else int(((200_000-i)/0.23)/60_000) for i in event_count_dict])
     runs2Basked = HowManyRuns(event_count_dict)
-    for typ in runs2Basked:
-        print("SETTING TO ONLY 1 RUN")
-        runs2Basked[typ]=1
+    # for typ in runs2Basked:
+    #     print("SETTING TO ONLY 1 RUN")
+    #     runs2Basked[typ]=1
     # runs2Basked = {}
     print("Runs to be asked: ", runs2Basked, sep='\n')
     print("event count: ", event_count_dict, sep='\n')
