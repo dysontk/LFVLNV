5ecf3ccdfe5f385f81098f0e5ef5903eeda9862f
dysontk <dkennedy@umass.edu>
2025-01-06 15:15:02 -0700

    analyxrmany only WZ, ZZ, W3

--- AnalysisAndSuch/AnalyzeMany.py
+++ AnalysisAndSuch/AnalyzeMany.py
@@ -82,9 +82,9 @@ def get_significance(cutNum):
 def main():
     list_of_data_types = [
         # 'LNVF',
+        'W3j',
         'WZ2j',
         'ZZ2j',
-        'W3j',
         # 'ttbar'
         ]
 
