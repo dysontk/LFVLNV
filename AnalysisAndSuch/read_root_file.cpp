78685f2508737da5875e93b568b7ae08302c304b
dysontk <dkennedy@umass.edu>
2025-02-06 10:15:51 -0700

    read root file else

--- AnalysisAndSuch/read_root_file.cpp
+++ AnalysisAndSuch/read_root_file.cpp
@@ -46,7 +46,7 @@ int main(int argc, char** argv) {
             }
         }
         else{
-            cout << "Something is wrong"
+            std::cout << "Something is wrong"
         }
     }
 
