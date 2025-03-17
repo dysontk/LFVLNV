79e9ef938610bbc450737a2636db736e43c34ee9
dysontk <dkennedy@umass.edu>
2025-03-17 13:27:26 -0600

    main printing efficiency

--- AnalysisAndSuch/JetFake/main.cpp
+++ AnalysisAndSuch/JetFake/main.cpp
@@ -1046,7 +1046,7 @@ int main(int argc, const char * argv[])
         cout << deepCuts2[10] << endl << NumEntries << endl;
         long efficency = deepCuts2[10]/NumEntries;
         cout << setprecision(7);
-        cout << "Efficiency: " << efficency << endl;
+        cout << "Efficiency: " << deepCuts2[10]/10 << endl;
     }
 
 }
