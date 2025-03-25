2d31cf7abf45d8baebb11fcfc7836d94f527fd93
dysontk <dkennedy@umass.edu>
2025-03-25 10:47:04 -0600

    fixed folder formatting: lambda integers. testing function that'll keep  folders it would otherwise recreate and delete preexisting folders not in the grid

--- AnalysisAndSuch/Param_space_explorer.py
+++ AnalysisAndSuch/Param_space_explorer.py
@@ -82,8 +82,8 @@ def checkExistingRuns(thisLambda, thisgeff):
 
 def checkExistingFolders(Linf, ginf, VERB):
     
-    lambdas = np.array(range(Linf['bounds'][0], Linf['bounds'][1]+Linf['delta'], Linf['delta']))
-    geffs = np.array(range(ginf['bounds'][0], ginf['bounds'][1]+ginf['delta'], ginf['delta']))
+    lambdas = np.arange(Linf['bounds'][0], Linf['bounds'][1]+Linf['delta'], Linf['delta'])
+    geffs = np.arange(ginf['bounds'][0], ginf['bounds'][1]+ginf['delta'], ginf['delta'])
     if VERB:
         print(lambdas)
         print(geffs)
