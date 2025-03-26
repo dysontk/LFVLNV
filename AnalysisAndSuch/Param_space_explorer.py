76dd708c3fea66d80c83cfff0cd0b74800c018a7
dysontk <dkennedy@umass.edu>
2025-03-26 10:07:14 -0600

    fixed folder formatting: lambda integers. testing function that'll keep  folders it would otherwise recreate and delete preexisting folders not in the grid

--- AnalysisAndSuch/Param_space_explorer.py
+++ AnalysisAndSuch/Param_space_explorer.py
@@ -149,6 +149,7 @@ def main():
             if not existingRuns:
                 gen_proc_command = '/home/dkennedy_umass_edu/Software/MG5_aMC_v3_5_6/bin/mg5_aMC ' + path_to_process_card
                 # GM.run_command(gen_proc_command)
+                print(f"no existing runs for {LambdaInfo['current']}, {geffInfo['current']}")
             else:
                 print(f"There is already a process for {LambdaInfo['current']} and {geffInfo['current']} with {existingRuns} runs")
             # howManyRuns = nRuns - existingRuns
