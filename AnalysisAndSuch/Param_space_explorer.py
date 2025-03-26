9f323be875c25e0fb2e2d1fb25c22d2c9953dff5
dysontk <dkennedy@umass.edu>
2025-03-26 11:42:11 -0600

    troubleshooting

--- AnalysisAndSuch/Param_space_explorer.py
+++ AnalysisAndSuch/Param_space_explorer.py
@@ -165,7 +165,7 @@ def main():
             path_to_process_card = edit_proc(LambdaInfo, geffInfo)
             if not existingRuns:
                 gen_proc_command = '/home/dkennedy_umass_edu/Software/MG5_aMC_v3_5_6/bin/mg5_aMC ' + path_to_process_card
-                # GM.run_command(gen_proc_command)
+                GM.run_command(gen_proc_command)
                 print(f"no existing runs for {LambdaInfo['current']}, {geffInfo['current']}")
             else:
                 print(f"There is already a process for {LambdaInfo['current']} and {geffInfo['current']} with {existingRuns} runs")
