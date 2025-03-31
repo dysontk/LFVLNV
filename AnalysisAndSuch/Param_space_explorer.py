b94238946c80513d8ca565bcee0b8b71f4596bd4
dysontk <dkennedy@umass.edu>
2025-03-31 10:39:03 -0600

    troubleshooting

--- AnalysisAndSuch/Param_space_explorer.py
+++ AnalysisAndSuch/Param_space_explorer.py
@@ -53,13 +53,14 @@ def edit_params(LInf, gInf, mass_r):
     
     return newLambdaInfo, newgeffInfo
 
-def edit_proc(LInfo, gInfo):
+def edit_proc(LInfo, gInfo, VRB=False):
     procPath = '/home/dkennedy_umass_edu/LNV/MyFiles/LFVLNV/GenerationFiles/LNVF_proc.dat' if not testing else 'test.dat'
     formatted_geff = '_'.join('{:.3f}'.format(gInfo['current']).split('.'))
     to_write = PT.procText + '_' + str(int(LInfo['current'])) + '_' + formatted_geff
 
     with open(procPath, 'w') as file:
-        print(to_write)
+        if VRB:
+            print(to_write)
         file.write(to_write)
     return procPath
 
