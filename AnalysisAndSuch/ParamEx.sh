aa211e46a527a295a2bcbcff21ee3c86c32a7826
dysontk <dkennedy@umass.edu>
2025-04-03 15:05:01 -0600

    removed 40hr time limit

--- AnalysisAndSuch/ParamEx.sh
+++ AnalysisAndSuch/ParamEx.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1 -n 1 -c 8
-#SBATCH --mem=8192  # Requested Memory
-#SBATCH -p gpu  # Partition
-#SBATCH -G 1  # Number of GPUs
+#SBATCH --mem=10000  # Requested Memory
+#SBATCH -p cpu  # Partition
 #SBATCH -q long #14 day time limit
-#SBATCH -t 40:00:00  # Job time limit
+#SBATCH -timelimit=14-0 # Job time limit
 #SBATCH -o slurm-%j.out  # %j = job ID
 
 export OMP_NUM_THREADS=8
