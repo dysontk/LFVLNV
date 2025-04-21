60fed8a148e7ee6086a5ffff72c622fa1a4f4932
dysontk <dkennedy@umass.edu>
2025-04-21 13:38:47 -0600

    read from text file to determine whether to skip the grid point

--- AnalysisAndSuch/Param_space_explorer.py
+++ AnalysisAndSuch/Param_space_explorer.py
@@ -226,7 +226,7 @@ def pull_existing_output():
     proclines = np.array([[]])
     with open('ParamSpEff.dat', 'r') as file:
         lines = file.readlines()
-        print("hi ", lines)
+        # print("hi ", lines)
         # proclines = np.array([[float(line.strip().split(' ')[i]) if i<2 else int(line.strip().split(' ')[i]) for i in range(len(line.strip().split(' ')))] for line in lines])
         proclines = np.array([[float(line.strip().split(' ')[i]) for i in range(len(line.strip().split(' ')))] for line in lines])
     
@@ -247,8 +247,8 @@ def main():
     print(f'Running a grid from g_eff = {geffInfo["bounds"][0]} to g_eff = {geffInfo["bounds"][1]}\n with step size δg_eff = {geffInfo["delta"]}')
     print(f"That's a grid of size {(LambdaInfo['bounds'][1]+LambdaInfo['delta']-LambdaInfo['bounds'][0])/LambdaInfo['delta'] * (geffInfo['bounds'][1]+geffInfo['delta']-geffInfo['bounds'][0])/geffInfo['delta']}")
     prev_out = pull_existing_output()
-    return 0
     print("previous: \n", prev_out)
+    return 0
     checkExistingFolders(LambdaInfo, geffInfo, True)
     sgrid = make_safe_grid()
     # return 0
