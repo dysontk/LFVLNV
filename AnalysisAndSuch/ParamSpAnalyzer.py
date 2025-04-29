d32e5b9da799d0176e2a7a04d7498351f5454a5f
dysontk <dkennedy@umass.edu>
2025-04-29 12:56:09 -0600

    added functionality to pull crossx for each grid point and write it to a file crossXs.dat

--- AnalysisAndSuch/ParamSpAnalyzer.py
+++ AnalysisAndSuch/ParamSpAnalyzer.py
@@ -25,7 +25,7 @@ def fileNameMaker(Lambda, geff):
     return f'LNVF_{int(Lambda)}_' + '_'.join('{:.3f}'.format(geff).split('.'))
 
 def get_crossX_from_html(filename):
-    print(filename)
+    print("checking ", filename)
     with open(filename, 'r') as file:
     # print(file.read())
         content = file.read()
@@ -35,8 +35,9 @@ def get_crossX_from_html(filename):
             if content[i:i+12] == 'results.html':
                 print(content[i:i+30])
                 howmanyResultLinesFound+=1
-                print('found it')
+                # print('found it')
                 if howmanyResultLinesFound > 1:
+                    print('found it')
                     j, carrotIndex1, carrotIndex2 = (i+12, i+12, i+12)
                     while content[j] != '<':
                         if content[j] == '>':
